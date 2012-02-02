d34f63704c90f938370aa71358a2fc987c9f4d83
finger.k4 <finger.k4@gmail.com>
2012-02-02 15:04:30 +0000

    Forgot something :)

--- MobileClickersWebModule/src/java/gr/academic/city/msc/industrial/mobileclickers/ejb/webservices/MobileClickersREST.java
+++ MobileClickersWebModule/src/java/gr/academic/city/msc/industrial/mobileclickers/ejb/webservices/MobileClickersREST.java
@@ -8,12 +8,9 @@ import gr.academic.city.msc.industrial.mobileclickers.ejb.exception.QuestionExce
 import gr.academic.city.msc.industrial.mobileclickers.ejb.session.QuestionService;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -39,7 +36,7 @@ public class MobileClickersREST {
         }
     }
 
-    @GET
+    @POST
     @Path("{questionCode}")
     public Response getNumberOfAnswer(@PathParam("questionCode") String questionCode) {
         try {
